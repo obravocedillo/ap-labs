2ff06736e64602450d2691333ecb2dcd648a65dd
Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>
2020-08-23 14:56:07 +0000

    Solve c-basics lab
    Signed-off-by: Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
+float calculateCelsius(int number){
+	float result = ((number-32)*5)/(float)9;
+	return result;
+}
 
-/* print Fahrenheit-Celsius table */
+int main(int argc, char* argv[]){
+	if(argc == 1){
+		printf("Ingrese una cantidad valida");
+	}else if(argc == 2){
+		int fahrenheit = atoi(argv[1]);
+		printf("Fahrenheit: %s, Celsius: %.1f",argv[1],calculateCelsius(fahrenheit));
+	}else if(argc == 4){
+		int start = atoi(argv[1]);
+		int end = atoi(argv[2]);
+		int increment = atoi(argv[3]);
+		for(int i=start; i<=end; i += increment){
+			printf("Fahrenheit: %d, Celcius: %.1f",i,calculateCelsius(i));
+			printf("\n");
+		}
 
-int main()
-{
-    int fahr;
-
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
-    return 0;
+	}
+	printf("\n");
+	return 0;
 }
