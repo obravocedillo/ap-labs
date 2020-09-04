a251e3dde45cb11760b0cb5fcaafd072d29f2fb5
Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>
2020-09-04 23:45:42 +0000

    Solve str-lib lab
    Signed-off-by: Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    return 0;
+int mystrlen(char *);
+char *mystradd(char *,char *);
+char mystrfind(char *,char *);
+int main(int argc, char* argv[]){
+	if(argc == 4){
+		if(strcmp(argv[1], "-add") == 0){
+			char *result = mystradd(argv[2],argv[3]);
+			printf("Initial Length: %d\n", mystrlen(argv[2]));
+			printf("New String: %s\n", result);
+			printf("New length: %d\n", mystrlen(result));
+			printf("\n");
+		}else if(strcmp(argv[1], "-find") == 0){
+			int result = mystrfind(argv[2],argv[3]);
+			if(result == -1){
+				printf("String was not found");
+				printf("\n");
+			}else{
+				printf("The string was found in position %d", result);
+				printf("\n");
+			}
+			printf("\n");
+		}
+    		return 0;
+	}else{
+		printf("Error, missing parameters\n");
+		printf("\n");
+	}
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,35 @@
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int mystrlen(char *str){
-    return 0;
+    int i;
+    for(i=0; str[i]!='\0'; i++);
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *result = malloc(mystrlen(origin) + mystrlen(addition) + 1);
+    strcpy(result, origin);
+    strcat(result, addition);
+    return result; 
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int i;
+    int j;
+    int originl = mystrlen(origin);
+    int substrl = mystrlen(substr);
+    int difference = originl-substrl;
+    for(i=0; i<=difference; i++){
+	    for(j = 0; j<substrl;j++){
+		if(origin[i + j] != substr[j]){
+			break;
+		}
+	    }
+	    if(j == substrl){
+		return i;
+	    }
+    }
+    return i;
 }
