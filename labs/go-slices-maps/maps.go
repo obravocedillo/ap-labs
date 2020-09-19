0cdabf4580455ee7a20eab7dd70c0c5cadc86962
Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>
2020-09-19 05:01:16 +0000

    Solve go-slices-map
    Signed-off-by: Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import(	"fmt" 
+		"strings"
+	  )	
+
+func wordCount(word string) map[string]int {
+	slice := make(map[string]int)
+	singleWords := strings.Fields(word)
+	for i := 0; i < len(singleWords); i++{
+		slice[singleWords[i]] += 1
+	}
+	return slice
+}
+
+
 func main() {
+	fmt.Printf("%v\n",wordCount("hola papu"))
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,20 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	slice := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		slice[i] = make([]uint8, dx);
+	}
+
+	for j := 0; j < dy; j++{
+		//position i in slice (aray of uint8)
+		for k := 0; k < len(slice[j]); k++{
+			//j ^ k proposed by golang tour page
+			slice[j][k] = uint8(j) ^ uint8(k)
+		}
+	}
+
+    return slice
 }
 
 func main() {
