a571cb1b8d619d900a1a2a24c4d6b7cd7721b53d
Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>
2020-11-04 23:50:30 +0000

    Solve goroutines-vs-os-threads lab
    Signed-off-by: Oliver Brandon Bravo Cedillo <A01229273@itesm.mx>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,45 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var commsPerSecond int
+var commsPerSecond = 0
 
+func ping(receiver chan int, sender chan int){
+	for{
+		//sends message to pong channel
+		receiver <- 10
+		//Add one to communications
+		commsPerSecond += 1
+		//Whait until message arrives
+		<-sender
+	}
+		
+	
+}
+
+func pong(receiver chan int, sender chan int){
+	for{
+		//Pong receives message
+		<-sender
+		//Pong sends message back to ping
+		receiver <- 10
+		//Add one to communications
+		commsPerSecond += 1
+	}
+}
+
+func main() {
+	chPing := make(chan int)
+	chPong := make(chan int)
+	//Start go routine 1
+	go ping(chPong,chPing)
+	//Start go routine 2
+	go pong(chPing, chPong)
+	//Sleep for one second
+	time.Sleep(1 * time.Second)
+	//Prints the number of communications
 	fmt.Println("Communications Per Second : ", commsPerSecond)
+	//Orfanes go routines die after main returns
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,51 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"runtime"
+	"log"
 )
 
-func main() {
-	var maxStages, transitTime int
+var maxStages int
+//Create first pipe global variable
+var firstPipe chan int
+var m runtime.MemStats
+			
+//Create pipelines equal to the number of n
+func createPipelines(n int) (<-chan int){
+	//Create channel for first pipe
+	firstPipe = make(chan int)
+	finalPipe := firstPipe
+	for i := 0; i < n; i++ {
+		runtime.ReadMemStats(&m)
+		maxStages ++
+		//Previous and next pipe of current i pipe
+		beforePipe := finalPipe
+		afterPipe := make(chan int)
+		go func(){
+			//Allow sending data to pipe
+			for data := range beforePipe {
+				afterPipe <- data
+			}
+		}()
+		//Final pipe of the for loop
+		finalPipe = afterPipe
+	}
+	return finalPipe
+}
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func main(){
+	lastPipe := createPipelines(9999)
+	startingTime := time.Now()
+	//Send message to first pipe
+	firstPipe <- 1
+	//Whait until last pipe receives the message
+	<-lastPipe
+	endTime := time.Now()
+	//Gets the difference from 2 times
+	transitTime := endTime.Sub(startingTime)
+	log.Printf("\nTotalAlloc = %v\nSys = %v\n\n", m.TotalAlloc/1024, m.Sys/1024)
+	fmt.Println("Maximum number of pipeline stages: ", maxStages)
+	fmt.Println("Total time to traverse the entire pipeline: ", transitTime)
+	fmt.Println("I decreased the number of pipelines to have a slower running time, my virtual box could use up to 99999 pipes")
 }
